391b382411ab0d1c41f3f569c962baad75030a81
Matt Strenk <strenkml@users.noreply.github.com>
2022-07-11 15:40:49 -0400

    Add logic for configuring the teams

--- Offense_Defense/Client/ClientMain.cs
+++ Offense_Defense/Client/ClientMain.cs
@@ -7,93 +7,97 @@ using Newtonsoft.Json;
 
 namespace OffenseDefense.Client
 {
-    public class ClientMain : BaseScript
+  public class ClientMain : BaseScript
+  {
+    readonly string[] teamColors = { "blue", "red", "green", "orange", "yellow", "pink", "purple", "white" };
+    dynamic teams;
+
+    // Timers
+    const int menuUpdateThreshold = 2000;
+    int menuUpdateTime = 0;
+    public ClientMain()
     {
-        readonly string[] teamColors = {"blue", "red", "green", "orange", "yellow", "pink", "purple", "white"};
-        List<Team> teams;
-        public ClientMain()
-        {
-            Debug.WriteLine("Hi from OffenseDefense.Client!");
-            
-            // Init the teams info
-            teams = new List<Team>();
-            foreach(string color in teamColors) {
-                teams.Add(new Team(color));
-            }
-            
-            // Commands
-            API.RegisterCommand("test", new Action(TestCommand), false);
-            API.RegisterCommand("del", new Action(DeleteCars), false);
-            API.RegisterCommand("show", new Action(ShowMenu), false);
-            API.RegisterCommand("hide", new Action(HideMenu), false);
-            API.RegisterCommand("joinTeam", new Action<String>(JoinTeam), false); 
+      Debug.WriteLine("Hi from OffenseDefense.Client!");
 
-            // NUI Callbacks
-            // API.RegisterNuiCallbackType("exit");
-            // EventHandlers["__cfx_nui:exit"] += new Action(() =>
-            // {
-            //     HideMenu();
-            // });
+      // Commands
+      API.RegisterCommand("test", new Action(TestCommand), false);
+      API.RegisterCommand("del", new Action(DeleteCars), false);
+      API.RegisterCommand("show", new Action(ShowMenu), false);
+      API.RegisterCommand("hide", new Action(HideMenu), false);
+      API.RegisterCommand("joinTeam", new Action<String>(JoinTeam), false);
 
-        }
+      // NUI Callbacks
+      // API.RegisterNuiCallbackType("exit");
+      // EventHandlers["__cfx_nui:exit"] += new Action(() =>
+      // {
+      //     HideMenu();
+      // });
 
-        private async void TestCommand() {
-            Ped player = Game.Player.Character;
-            API.RequestModel((uint) VehicleHash.Banshee);
-            Vehicle car = await World.CreateVehicle(VehicleHash.Apc, player.Position + (player.ForwardVector * 2));
-            car.Mods.PrimaryColor = VehicleColor.Chrome;
-        }
+      // Event Handlers
+      EventHandlers.Add("OffDef:UpdateTeams", new Action<dynamic>(UpdateTeams));
 
-        private void DeleteCars() {
-            Vehicle[] cars = World.GetAllVehicles();
-            foreach(Vehicle car in cars) {
-                car.Delete();
-            }
-        }
-
-        private async void ShowMenu() {
-            // dynamic playerIds = API.GetActivePlayers();
-            // List<string> playerNames = new List<string>();
-            // for (int i = 0; i < playerIds.Count; i++) {
-            //     playerNames.Add(API.GetPlayerName(playerIds[i]));
-            // }
-            API.SendNuiMessage(JsonConvert.SerializeObject(new {enable = true}));
-            API.SetNuiFocus(false, false);
-        }
-        private void HideMenu() {
-            API.SendNuiMessage(JsonConvert.SerializeObject(new {enable = false}));
-            API.SetNuiFocus(false, false);
-        }
-
-        private void UpdateMenu() {
-            API.SendNuiMessage(JsonConvert.SerializeObject(new {teams = teams}));
-        }
-
-        private void JoinTeam(String teamColor) {
-            // Current issue is finding a player on a team when they are switching teams
-            // The team data structure needs to be rethought
-            string playerName = Game.Player.Name;
-            Team selectedTeam = teams.Find(e => e.GetColor() == teamColor);
-            List<string> oldBlockers = selectedTeam.GetBlockers();
-            List<string> newBlockers = new List<string>();
-            foreach(string blocker in oldBlockers) {
-                if (blocker == playerName) {
-                    // Already part of the team
-                    // Do nothing or send error to player
-                    return;
-                } else {
-                    newBlockers.Add(blocker);
-                }
-            }
-        }
-
-
-        [Tick]
-        public Task OnTick()
-        {
-            // DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
-            
-            return Task.FromResult(0);
-        }
     }
+
+    private async void TestCommand()
+    {
+      Ped player = Game.Player.Character;
+      API.RequestModel((uint)VehicleHash.Banshee);
+      Vehicle car = await World.CreateVehicle(VehicleHash.Apc, player.Position + (player.ForwardVector * 2));
+      car.Mods.PrimaryColor = VehicleColor.Chrome;
+    }
+
+    private void DeleteCars()
+    {
+      Vehicle[] cars = World.GetAllVehicles();
+      foreach (Vehicle car in cars)
+      {
+        car.Delete();
+      }
+    }
+
+    private async void ShowMenu()
+    {
+      API.SendNuiMessage(JsonConvert.SerializeObject(new { enable = true }));
+      API.SetNuiFocus(false, false);
+    }
+    private void HideMenu()
+    {
+      API.SendNuiMessage(JsonConvert.SerializeObject(new { enable = false }));
+      API.SetNuiFocus(false, false);
+    }
+
+    private void UpdateMenu()
+    {
+      API.SendNuiMessage(JsonConvert.SerializeObject(new { teams = teams }));
+    }
+
+    private void JoinTeam(String teamColor)
+    {
+      string playerName = Game.Player.Name;
+
+      API.TriggerServerEvent("OffDef:AddPlayer", teamColor, playerName);
+    }
+
+    private void UpdateTeams(dynamic teams)
+    {
+      this.teams = teams;
+      UpdateMenu();
+    }
+
+
+    [Tick]
+    public Task OnTick()
+    {
+      if (menuUpdateTime >= menuUpdateThreshold)
+      {
+        menuUpdateTime = 0;
+        UpdateMenu();
+      }
+      else
+      {
+        menuUpdateTime++;
+      }
+      return Task.FromResult(0);
+    }
+  }
 }
--- Offense_Defense/Server/ServerMain.cs
+++ Offense_Defense/Server/ServerMain.cs
@@ -1,21 +1,85 @@
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using CitizenFX.Core;
 using CitizenFX.Core.Native;
 
 namespace OffenseDefense.Server
 {
-    public class ServerMain : BaseScript
+  public class ServerMain : BaseScript
+  {
+    readonly string[] teamColors = { "blue", "red", "green", "orange", "yellow", "pink", "purple", "white" };
+    Dictionary<string, Team> teams = new Dictionary<string, Team>();
+    public ServerMain()
     {
-        public ServerMain()
-        {
-            Debug.WriteLine("Hi from OffenseDefense.Server!");
-        }
+      Debug.WriteLine("Hi from OffenseDefense.Server!");
 
-        [Command("hello_server")]
-        public void HelloServer()
-        {
-            Debug.WriteLine("Sure, hello.");
-        }
+      foreach (string color in teamColors)
+      {
+        teams.Add(color, new Team(color));
+      }
+
+      // Event Handlers
+      EventHandlers.Add("OffDef:AddPlayer", new Action<string, string>(AddPlayer));
+      EventHandlers.Add("OffDef:RemovePlayer", new Action<string, string>(RemovePlayer));
+      EventHandlers.Add("OffDef:SetRunner", new Action<string, string>(SetRunner));
+
+
     }
+
+    // The rest of the teams need to be checked to see if the player is on their team
+    private void AddPlayer(string color, string name)
+    {
+      string otherTeam = IsPlayerInOtherTeam(color, name);
+      if (otherTeam != "")
+      {
+        Team oldTeam = this.teams[otherTeam];
+        oldTeam.RemovePlayer(name);
+      }
+      Team team = this.teams[color];
+      team.AddPlayer(name);
+      API.TriggerClientEvent("OffDef:UpdateTeams", this.teams);
+    }
+
+    private void RemovePlayer(string color, string name)
+    {
+      Team team = this.teams[color];
+      team.RemovePlayer(name);
+      API.TriggerClientEvent("OffDef:UpdateTeams", this.teams);
+    }
+
+    private void SetRunner(string color, string name)
+    {
+      Team team = this.teams[color];
+      bool result = team.SetRole(name, "Runner");
+      if (!result)
+      {
+        Debug.WriteLine($"ERROR: Failed to set player {name} to runner for {color} team");
+      }
+      API.TriggerClientEvent("OffDef:UpdateTeams", this.teams);
+    }
+
+    private string IsPlayerInOtherTeam(string excludeColor, string name)
+    {
+      foreach (KeyValuePair<string, Team> entry in this.teams)
+      {
+        if (entry.Key != excludeColor)
+        {
+          if (entry.Value.IsPlayer(name))
+          {
+            return entry.Key;
+          }
+        }
+      }
+      return "";
+    }
+
+
+
+    [Command("hello_server")]
+    public void HelloServer()
+    {
+      Debug.WriteLine("Sure, hello.");
+    }
+  }
 }
